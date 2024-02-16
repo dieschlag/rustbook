66843716fce2110718c7d75c5e51d8f8dbd06c0b
mateo.dischler <mateo.dischler@student-cs.fr>
2024-02-16 23:04:27 +0100

    première utilisation cargo build --version

---
+++ projects/hello_cargo/target/release/deps/hello_cargo-ac6d8d77e18d4d6b.d
@@ -0,0 +1,5 @@
+/home/dieschlag/Rust/Rustbook/projects/hello_cargo/target/release/deps/hello_cargo-ac6d8d77e18d4d6b: src/main.rs
+
+/home/dieschlag/Rust/Rustbook/projects/hello_cargo/target/release/deps/hello_cargo-ac6d8d77e18d4d6b.d: src/main.rs
+
+src/main.rs:
---
+++ projects/hello_cargo/target/release/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/dieschlag/Rust/Rustbook/projects/hello_cargo/target/release/hello_cargo: /home/dieschlag/Rust/Rustbook/projects/hello_cargo/src/main.rs
