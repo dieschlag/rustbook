55bbb30c708d4365035642ae1d449f8c32390eba
mateo.dischler <mateo.dischler@student-cs.fr>
2024-02-23 11:43:21 +0100

    several exemples of ownership and scope examples

---
+++ projects/l4_1_what_is_ownership/target/debug/deps/l4_1_what_is_ownership-bc8e5ce6c6fcfb0c.d
@@ -0,0 +1,5 @@
+/home/dieschlag/Rust/Rustbook/projects/l4_1_what_is_ownership/target/debug/deps/l4_1_what_is_ownership-bc8e5ce6c6fcfb0c: src/main.rs
+
+/home/dieschlag/Rust/Rustbook/projects/l4_1_what_is_ownership/target/debug/deps/l4_1_what_is_ownership-bc8e5ce6c6fcfb0c.d: src/main.rs
+
+src/main.rs:
---
+++ projects/l4_1_what_is_ownership/target/debug/l4_1_what_is_ownership.d
@@ -0,0 +1,1 @@
+/home/dieschlag/Rust/Rustbook/projects/l4_1_what_is_ownership/target/debug/l4_1_what_is_ownership: /home/dieschlag/Rust/Rustbook/projects/l4_1_what_is_ownership/src/main.rs
