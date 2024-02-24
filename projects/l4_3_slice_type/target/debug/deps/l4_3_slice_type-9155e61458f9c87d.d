a32b2bbdf19aa46b014169d09381321c72d62a29
mateo.dischler <mateo.dischler@student-cs.fr>
2024-02-24 11:40:09 +0100

    examples to understand slices

---
+++ projects/l4_3_slice_type/target/debug/deps/l4_3_slice_type-9155e61458f9c87d.d
@@ -0,0 +1,5 @@
+/home/dieschlag/Rust/Rustbook/projects/l4_3_slice_type/target/debug/deps/l4_3_slice_type-9155e61458f9c87d: src/main.rs
+
+/home/dieschlag/Rust/Rustbook/projects/l4_3_slice_type/target/debug/deps/l4_3_slice_type-9155e61458f9c87d.d: src/main.rs
+
+src/main.rs:
---
+++ projects/l4_3_slice_type/target/debug/l4_3_slice_type.d
@@ -0,0 +1,1 @@
+/home/dieschlag/Rust/Rustbook/projects/l4_3_slice_type/target/debug/l4_3_slice_type: /home/dieschlag/Rust/Rustbook/projects/l4_3_slice_type/src/main.rs
